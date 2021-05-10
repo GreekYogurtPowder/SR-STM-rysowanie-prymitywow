c7bd8034c19f7860752c811fd6b569e166275735
carleoner <kubaw4000@gmail.com>
2021-05-10 22:54:26 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,6 +119,7 @@ int main(void)
   BSP_LCD_DisplayOn();                                                //Wlaczenie podswietlania
   BSP_LCD_Clear(LCD_COLOR_WHITE);                                     //Kolor Tla
 
+  //test
   //wykorzystanie naszej biblioteki
   //drawingHLine(200, 100, 200);
   //drawingVLine(200, 200, 200);
